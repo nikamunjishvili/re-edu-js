589b6dd24aaebe8141da802e542ae4a37699df6e
Nika munjisvili <nikamunja31415@gmail.com>
2024-01-11 21:23:52 +0400

    add commut

---
+++ lecturer_3/app.js
@@ -0,0 +1,231 @@
+// // let input = "nick12345";
+
+// // console.log(input.length);
+// // if (input.length === "") {
+// //   console.log("plz write something...");
+// // } else if (false) {
+// //   console.log("bla bla ");
+// // } else {
+// //   console.log("got it!!");
+// // }
+
+// //დაწერეთ ლოგიკა რომლლის მიხედვითაც გამოითვლით თუ
+// // ინფუტში შეყვანილი მონაცემი
+// // მეტია 20 დაწეროს "error"
+// //თუ ნაკლებია 20 ამ შემთხვევაში დაგვარეგისტრიროს
+// // თუ 10 ნაკლებია მონაცემი ინფუტში მაგ შემთხვევაში
+// //დავწეროთ რომ უნდა იყოს 10 მეტი
+
+// // let firstName = "Nick";
+// // let lastName = "Munjishvili";
+// // let email = "nick123@gmail.com";
+
+// // if (firstName !== "" && lastName !== "" && email !== "") {
+// //   firstName = "Gega";
+// //   console.log("changed name", firstName);
+// //   console.log("Wellcome");
+// // } else {
+// //   console.log("User not found!!");
+// // }
+
+// // let input = Number(prompt("enter something..."));
+
+// // if (input % 2 === 0) {
+// //   alert("this number is even");
+// // } else if (input % 2 !== 0) {
+// //   alert("this number is odd!");
+// // } else {
+// //   alert("Error!!");
+// // }
+
+// // if (false) console.log("hello");
+// // else console.log("error");
+
+// let names = "dsdsdss";
+
+// switch (names) {
+//   case "nika":
+//     console.log("Hello Nika");
+//     break;
+//   case "sandro":
+//     console.log("Hello Sandro!!");
+//     break;
+//   case "luka":
+//     console.log("Hello Luka!!");
+//     break;
+//   default:
+//     console.log("Error!!");
+//     break;
+// }
+
+// // let age = prompt("Tipe your age");
+
+// // if (age > 20) {
+// //   console.log("welcome");
+// // } else if (age > 5 && age < 13) {
+// //   console.log("ჯერ ბავშვი ხარ");
+// // } else if (age > 13 && age < 20) {
+// //   console.log("ჯერ თინეიჯერი ხარ");
+// // } else {
+// //   console.log("error");
+// // }
+
+// let age = prompt("რამდენი წლის ხართ?");
+// if (age >= 20) {
+//   console.log("რეგისტრაცია წარმატებულია");
+// } else if (age >= 13 && age < 20) {
+//   console.log("თინეიჯერი");
+// } else if (age >= 5 && age < 13) {
+//   console.log("ბავშვი");
+// } else {
+//   console.log("ERROR");
+// }
+
+// let input = prompt("შეიყვანე რამე");
+// if (input == 0) {
+//   console.log("ERROR");
+// } else if (input % 2 == 0) {
+//   console.log("ლუწია");
+// } else {
+//   console.log("კენტი");
+// }
+
+// let input = Number(prompt("enter something..."));
+
+// if (input % 2 === 0) {
+//   console.log("Even");
+// } else if (input % 2 !== 0) {
+//   console.log("odd!!");
+// } else {
+//   console.log("Error!!");
+// }
+
+// let age = prompt("რამდენი წლის ხარ?");
+
+// if (age > 20) {
+//   console.log("შენ დარეგისტრირდი წარმატებულად!");
+// } else if (age >= 13 && age <= 20) {
+//   console.log("შენ ჯერ კიდევ თინეიჯერი ხარ!");
+// } else {
+//   age <= 13 && age >= 5;
+//   console.log("შენ ჯერ კიდევ ბავშვი ხარ!");
+// }
+
+// let num = Number(prompt("enter numder"));
+
+// if (num % 2 === 0) {
+//   console.log("even");
+// } else if (num % 2 !== 0) {
+//   console.log("odd");
+// } else {
+//   console.log("eror");
+// }
+
+// let age = prompt("enter age");
+// if (age > 5 && age < 13) {
+//   console.log("არის ბავშვი");
+// } else if (age > 13 && age < 20) {
+//   console.log("არის თინეიჯერი!");
+// } else {
+//   console.log("არის ზრდასრული!!");
+// }
+
+// else{
+//     console.log("error!!")
+// }
+
+// let input = prompt("enter something...")
+// if(input% 2 === 0){console.log("ეს რიცხვი ლუწია")}
+// else if( input% 2 !== 0){console.log("ეს რიცხვი კენტია")}
+// else console.log("Error!!")
+
+// let age = 10;
+
+// if (age > 5 && age <= 13) {
+//   console.log("ბავშვი");
+// } else if (age > 13 && age <= 19) {
+//   console.log("თინეიჯერი");
+// } else if (age > 20) {
+//   console.log("adult");
+// } else if (age < 5) {
+//   console.log("child");
+// } else {
+//   console.log("error");
+// }
+
+// let number = "nika";
+
+// if (Number(number % 2 === 0)) {
+//   console.log("even");
+// } else if (Number(number % 2 !== 0)) {
+//   console.log("odd");
+// } else {
+//   console.log("error");
+// }
+
+// let age = Number(prompt("your age"));
+
+// if (age < 15) {
+//   console.log("bavshvi");
+// } else if (age > 15 && age < 20) {
+//   console.log("tineijeri");
+// } else {
+//   console.log("zrdasruli");
+// }
+
+// let age = prompt("type your age");
+
+// if (age >= 20) {
+//   console.log("welcome");
+// } else if (age > 5 && age < 13) {
+//   console.log("you are child");
+// } else {
+//   console.log("you are teeneger");
+// }
+
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+// let currentDate = new Date().getDay();
+// console.log(currentDate);
+
+// switch (currentDate) {
+//   case 0:
+//     console.log("კვირა");
+//     break;
+//   case 1:
+//     console.log("ორშაბათი");
+//     break;
+
+//   case 2:
+//     console.log("სამშაბათი");
+//     break;
+
+//   case 3:
+//     console.log("ოთხშაბათი");
+//     break;
+
+//   case 4:
+//     console.log("ხუთშაბათი");
+//     break;
+
+//   case 5:
+//     console.log("პარასკები");
+//     break;
+
+//   case 6:
+//     console.log("შაბათი");
+//     break;
+
+//   default:
+//     console.log("Error");
+//     break;
+// }
+
+
