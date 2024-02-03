28dd85f7ab1f6236d9492b6785c854b41a2abbc0
Nika munjisvili <nikamunja31415@gmail.com>
2024-02-03 13:46:48 +0400

    add

--- lecture_13/app.js
+++ lecture_13/app.js
@@ -91,59 +91,116 @@ const uniqueNum = new Set([]);
 // }
 // console.log(result);
 
-const user = [];
+// const user = [];
 
-const result = user.push(
-  {
-    id: 1,
-    name: "Nikusha",
-    age: 20,
-  },
-  {
-    id: 2,
-    name: "Luka",
-    age: 25,
-  }
-);
+// const result = user.push(
+//   {
+//     id: 1,
+//     name: "Nikusha",
+//     age: 20,
+//   },
+//   {
+//     id: 2,
+//     name: "Luka",
+//     age: 25,
+//   }
+// );
 
 // console.log(user);
 
-let userId = 2;
+// let userId = 2;
 
-const deleteUser = user.filter(delete_user => {
-    return delete_user.id !== userId;
-})
+// const deleteUser = user.filter((delete_user) => {
+//   return delete_user.id !== userId;
+// });
 
-console.log(deleteUser);
-// const isLogedin = user.some(user => user.id === userId);
+// console.log(deleteUser);
+// const isLogedin = user.some((user) => user.id === userId);
 
-// if(isLogedin){
-//     console.log(`User with ID ${userId} is logged in`);
-// }else{
-//     console.log(`No user with ID ${userId} is logged in`);
+// if (isLogedin) {
+//   console.log(`User with ID ${userId} is logged in`);
+// } else {
+//   console.log(`No user with ID ${userId} is logged in`);
 // }
 
-// for(const users of user){
-//     document.body.innerHTML = users.name
+// for (const users of user) {
+//   document.body.innerHTML = users.name;
 // }
 
-
-// const elements = [1,2,5,3,6];
+// const elements = [1, 2, 5, 3, 6];
 // delete elements[2];
 
 // console.log(elements);
 
+// const numbers = [12, 2, 43, 54, 656, 76, 8787, 56];
 
+// // const resultss = numbers.sort((a, b) => a - b);
 
+// // console.log(resultss);
 
-
-
-const numbers = [12,2,43,54,656,76,8787,56];
-
-// const resultss = numbers.sort((a, b) => a - b);
+// const resultss = numbers.reverse();
 
 // console.log(resultss);
 
-const resultss = numbers.reverse();
+//1)უნდა გვქონდეს მასივი რიცხვების მაგალითად: array = [1,2,5,43,5,5,64,54];
+//უნდა გავფლიტროთ და კენტები ახალ ცალკე მასივში შევინახოთ და ლუწი რიცხვები ახალ მასივში;
 
-console.log(resultss);
+
+// let arrey = [1,2,5,43,5,5,64,54]
+// let odd = []
+// let even = []
+
+// function filter() {
+//     arrey.forEach((num) => {
+//         if(num % 2 === 0){
+//             odd.push(num)
+//         }else{
+//             even.push(num)
+//         }
+//     })
+
+// }
+
+// filter()
+// console.log(odd)
+// console.log(even)
+
+
+let user = [12, 15, 82, 16, 21, 65]
+let user1 = [12,32,43];
+
+const result = user.concat(user1);
+console.log(result);
+
+// let newOddUser = []
+// let newEvenUser = []
+
+//  for (let i = 0; i < user.length; i++) {
+//     if (user[i] % 2 === 0) {
+//         newEvenUser.push(user[i])
+//     } else {
+//         newOddUser.push(user[i])
+//     }
+//  }
+
+//  console.log(newOddUser)
+//  console.log(newEvenUser)
+const array = [1, 2, 5, 43, 55, 64, 54];
+const odd = [];
+const even = [];
+
+function filternums() {
+    array.forEach(function(num) {
+        if (num % 2 === 0) {
+            even.push(num);
+        } else {
+            odd.push(num);
+        }
+    });
+}
+
+filternums(); 
+
+
+console.log(odd);
+console.log(even);
