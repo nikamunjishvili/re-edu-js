dc81dfafc585ade14579b59f69ab9a4e453dcefa
Nika munjisvili <nikamunja31415@gmail.com>
2024-03-09 12:05:59 +0400

    saved file

---
+++ lecture_23/app.js
@@ -0,0 +1,94 @@
+// // function generatePassword(length) {
+// //   let randomText = "dsdjsdnsdnsjfbsiojfdfndjsdmnjsfndf";
+// //   let password = "";
+// //   for (let i = 0; i < length; i++) {
+// //     password += randomText[Math.floor(Math.random() * randomText.length)];
+// //   }
+// //   return password;
+// // }
+
+// // console.log(generatePassword(15));
+
+// // const users_data = [
+// //   { id: 1, name: "Guja", isMarried: false, sallary: 200 },
+// //   { id: 2, name: "Luka", isMarried: true, sallary: 100 },
+// //   { id: 3, name: "Saba", isMarried: false, sallary: 500 },
+// // ];
+
+// // const result = users_data
+// //   .filter((user) => user.id !== 2)
+// //   .sort((a, b) => a - b)
+// //   .map((user) => {
+// //     return {
+// //       ...user,
+// //       address: "Tbilisi",
+// //     };
+// //   }).reduce((initialValue, user) => {
+// //     return initialValue + user.sallary
+// //   }, 0)
+
+// // console.log("updated data", result);
+
+// // class User {
+// //   constructor(title, author, year) {
+// //     this.title = title;
+// //     this.author = author;
+// //     this.year = year;
+// //   }
+
+// //   sayFullBookName() {
+// //     return `${this.title} - ${this.author} -  ${this.year}`;
+// //   }
+// // }
+
+// // class SayFullName extends Books {
+// //   sayFullBookName() {
+// //     return `${this.title} - ${this.author} - ${this.year}`
+// //   }
+// // }
+
+// // const book = new SayFullName("აი ია", "იაკობი", 2024);
+// // const result = book.sayFullBookName();
+// // console.log(result);
+
+
+// // firstname, lastname, email, address, passwords
+
+
+
+
+// // let a = 10;
+// // let b = 15;
+// // let c;
+// // console.log(`a:${a}- b:${b}`);
+// // c = a;
+// // a = b;
+// // b = c;
+
+// // console.log(`a:${a}- b:${b}`);
+
+
+// // let a = 10;
+// // let b = 15;
+// // console.log(`a:${a}- b:${b}`);
+
+// // a = a + b;
+// // b = a - b;
+// // a = a - b;
+
+// // console.log(`a:${a}- b:${b}`);
+
+
+// // console.log(process);
+
+
+// // const array = [1,2,3,4]
+
+
+// isFunction();
+
+
+
+// function isFunction(){
+    
+// }
